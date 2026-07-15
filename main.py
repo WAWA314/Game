0b4ef7388f5090c54ec152a7a296ca132672d039
WAWA314 <napat92005@gmail.com>
2026-07-15 11:09:49 +0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from src.Calpkg.gamepkg.game_center import GameApp
-def main():
-    root = tk.Tk()
-    app = GameApp(root)
-    root.mainloop()
-if __name__ == "__main__":
-    main()
